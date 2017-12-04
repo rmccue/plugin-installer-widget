d0fdd67387fcacf9328736503c9561f7604d7964
Ryan McCue <me@ryanmccue.info>
2017-12-04 17:47:24 +1000

    Allow http access

--- src/App.js
+++ src/App.js
@@ -101,6 +101,10 @@ export default class App extends Component {
 				/>
 
 				<p>
+					Note: To access users' sites on <code>http:</code>, your code <em>must</em> also be on <code>http</code>.
+				</p>
+
+				<p>
 					Need more customisation?
 					{ ' ' }
 					<a href="https://github.com/rmccue/plugin-installer-widget">Host your own</a>.
